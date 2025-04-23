a1df12c1cd8cfa17c249e2c4af5ab3b4475b83e0
nkthinh <thinhnkse171639@fpt.edu.vn>
2025-04-23 22:43:46 +0700

    edit bus and order page

--- src/pages/client/order.jsx
+++ src/pages/client/order.jsx
@@ -1,29 +1,28 @@
-import { formatToMoney, showError } from '@/utils';
-import React, { useEffect, useState } from 'react'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { useLocation } from 'react-router-dom';
+import { formatToMoney, showError } from "@/utils";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useLocation } from "react-router-dom";
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const dataTrip = {
   Id: 1,
-  RouterName: 'Hà Nội - Đà Nẵng',
-  BusName: 'Xe giường nằm 2 tầng',
-  DriverName: 'Tài xế Minh',
+  RouterName: "Hà Nội - Đà Nẵng",
+  BusName: "Xe giường nằm 2 tầng",
+  DriverName: "Tài xế Minh",
   Price: 300000,
   Status: 1,
-  DepartureTime: '10:00',
-  Date: '2024-01-01'
-}
+  DepartureTime: "10:00",
+  Date: "2024-01-01",
+};
 
 export default function OrderPage() {
   const location = useLocation();
-  const seats = new URLSearchParams(location.search).get('seat');
-  const seatsList = seats.split(',');
-  const trip = new URLSearchParams(location.search).get('trip');
+  const seats = new URLSearchParams(location.search).get("seat");
+  const seatsList = seats.split(",");
+  const trip = new URLSearchParams(location.search).get("trip");
   const [profile, setProfile] = useState();
 
-  const [tripDetail, setTripDetail] = useState(dataTrip)
+  const [tripDetail, setTripDetail] = useState(dataTrip);
   const {
     register,
     handleSubmit,
@@ -36,8 +35,8 @@ export default function OrderPage() {
     if (profile) {
       const parsedProfile = JSON.parse(profile);
       setProfile(parsedProfile);
-      setValue('name', profile.name)
-      setValue('phoneNumber', profile.phoneNumber)
+      setValue("name", profile.name);
+      setValue("phoneNumber", profile.phoneNumber);
     }
   }, []);
 
@@ -54,8 +53,8 @@ export default function OrderPage() {
     })
       .then(async (res) => {
         const result = await res.json();
-        if (res.status === 200) { }
-        else {
+        if (res.status === 200) {
+        } else {
           showError(result.message);
         }
       })
@@ -66,18 +65,22 @@ export default function OrderPage() {
 
   return (
     <div className="px-8 md:px-10 w-full max-w-[1200px] mx-auto py-10 my-20">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex max-md:flex-col w-full gap-6">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex max-md:flex-col w-full gap-6"
+      >
         <div className="flex flex-col w-2/3 gap-2 border shadow-md rounded-md p-4">
-          <p className='text-xl font-bold '>Thông tin liên hệ</p>
-          <div className='flex flex-col gap-2'>
+          <p className="text-xl font-bold ">Thông tin liên hệ</p>
+          <div className="flex flex-col gap-2">
             <div className="flex flex-col gap-1">
               <label className="text-sm">Tên người đi</label>
               <input
                 type="text"
                 placeholder="Nhập họ và tên..."
                 defaultValue={profile?.name}
-                className={`border px-5 py-2 rounded-lg ${errors.username ? "border-red-500" : "border-gray"
-                  }`}
+                className={`border px-5 py-2 rounded-lg ${
+                  errors.username ? "border-red-500" : "border-gray"
+                }`}
                 {...register("username", {
                   required: "Vui lòng nhập họ và tên người đi",
                   minLength: {
@@ -88,15 +91,14 @@ export default function OrderPage() {
               />
             </div>
             <div className="flex flex-col gap-2 mb-4 w-full">
-              <label className="text-sm">
-                Số điện thoại
-              </label>
+              <label className="text-sm">Số điện thoại</label>
               <input
                 inputMode="numeric"
                 placeholder="Nhập số điện thoại..."
                 defaultValue={profile?.phoneNumber}
-                className={`border px-5 py-2 w-full rounded-lg ${errors.phone ? "border-red-500" : "border-gray"
-                  }`}
+                className={`border px-5 py-2 w-full rounded-lg ${
+                  errors.phone ? "border-red-500" : "border-gray"
+                }`}
                 {...register("phone", {
                   required: "Vui lòng nhập số điện thoại",
                   minLength: {
@@ -119,50 +121,61 @@ export default function OrderPage() {
             </div>
           </div>
           <hr />
-          <p className='text-xl font-bold'>Chi tiết chuyến đi</p>
+          <p className="text-xl font-bold">Chi tiết chuyến đi</p>
           <div className="flex border shadow-sm p-4 rounded-md gap-2">
-            <img src="https://placehold.co/120" className='w-32 h-32 object-cover' />
+            <img
+              src="https://placehold.co/120"
+              className="w-32 h-32 object-cover"
+            />
             <div className=" flex flex-col">
-              <p className='text-lg font-bold'>{tripDetail?.RouterName}</p>
-              <p className=''>{tripDetail?.BusType ?? 'Xe giường nằm'}</p>
-              <p className=''>
+              <p className="text-lg font-bold">{tripDetail?.RouterName}</p>
+              <p className="">{tripDetail?.BusType ?? "Xe giường nằm"}</p>
+              <p className="">
                 <span className="font-semibold">Ngày khởi hành: </span>
-                {tripDetail?.Date}</p>
-              <p className=''>
+                {tripDetail?.Date}
+              </p>
+              <p className="">
                 <span className="font-semibold">Giờ khởi hành: </span>
-                {tripDetail?.DepartureTime}</p>
-              <p className=''>
+                {tripDetail?.DepartureTime}
+              </p>
+              <p className="">
                 <span className="font-semibold">Ghế: </span>
-                {seats}</p>
+                {seats}
+              </p>
             </div>
           </div>
         </div>
         <div className="flex flex-col md:w-[400px] gap-2 border shadow-md rounded-md p-4 h-fit sticky top-22">
-          <p className='text-xl font-bold'>Thanh toán</p>
+          <p className="text-xl font-bold">Thanh toán</p>
           <div className="flex flex-col gap-2">
             <div className="flex flex-row justify-between">
-              <p className='font-semibold text-lg'>Giá vé:</p>
-              <p className='text-lg'>{formatToMoney(tripDetail?.Price ?? 0)}</p>
+              <p className="font-semibold text-lg">Giá vé:</p>
+              <p className="text-lg">{formatToMoney(tripDetail?.Price ?? 0)}</p>
             </div>
             <div className="flex flex-row justify-between">
-              <p className='font-bold text-lg'>Số lượng ghế:</p>
-              <p className='text-lg'>{seatsList?.length ?? 1}</p>
+              <p className="font-bold text-lg">Số lượng ghế:</p>
+              <p className="text-lg">{seatsList?.length ?? 1}</p>
             </div>
             <hr />
             <div className="flex flex-row justify-between">
-              <p className='font-semibold text-lg'>Tổng tiền:</p>
-              <p className='text-lg'>{formatToMoney(tripDetail?.Price * seatsList?.length)}</p>
+              <p className="font-semibold text-lg">Tổng tiền:</p>
+              <p className="text-lg">
+                {formatToMoney(tripDetail?.Price * seatsList?.length)}
+              </p>
             </div>
             <div className="flex flex-row justify-between">
-              <p className='font-semibold text-lg'>Mã khuyến mãi: </p>
-              <input
-                className='p-2 h-[32px] border border-primary rounded-md max-w-[130px]'
-              />
+              <p className="font-semibold text-lg">Mã khuyến mãi: </p>
+              <input className="p-2 h-[32px] border border-primary rounded-md max-w-[130px]" />
             </div>
-            <button type="submit" className="bg-primary text-white font-bold py-2 mt-2 px-4 rounded-md">Thanh toán</button>
+            <button
+              type="submit"
+              className="bg-primary text-white font-bold py-2 mt-2 px-4 rounded-md"
+            >
+              Thanh toán
+            </button>
           </div>
         </div>
       </form>
     </div>
-  )
+  );
 }
