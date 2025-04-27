e90352a261c122e79e6937e3a771d6995dfab68b
nkthinh <thinhnkse171639@fpt.edu.vn>
2025-04-27 23:11:33 +0700

    update admin ticket confirm

--- src/pages/admin/ticket.jsx
+++ src/pages/admin/ticket.jsx
@@ -1,27 +1,27 @@
-import SidebarAdmin from '@/components/common/sidebar-admin';
-import CustomTable from '@/components/common/table';
-import { formatDateTime, formatToMoney, showError } from '@/utils';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import ReactPaginate from 'react-paginate';
+import SidebarAdmin from "@/components/common/sidebar-admin";
+import CustomTable from "@/components/common/table";
+import { formatDateTime, formatToMoney, showError, showSuccess } from "@/utils";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const columns = [
-  { header: 'Mã vé', field: 'tripId' },
-  { header: 'Tên tuyến đường', field: 'routerName' },
-  { header: 'Tên khách hàng', field: 'customerName' },
-  { header: 'Mã Ghế', field: 'seatId' },
-  { header: 'Giá vé', field: 'price' },
-  { header: 'Khởi tạo', field: 'createdAt' },
-  { header: 'Trạng thái', field: 'status', className: 'status-box' },
-]
+  { header: "Mã vé", field: "tripId" },
+  { header: "Tên tuyến đường", field: "routeName" },
+  { header: "Tên khách hàng", field: "customerName" },
+  { header: "Mã Ghế", field: "seatId" },
+  { header: "Giá vé", field: "price" },
+  { header: "Khởi tạo", field: "createdAt" },
+  { header: "Trạng thái", field: "status", className: "status-box" },
+];
 
 export default function TicketListPage() {
   const [dataList, setDataList] = useState([]);
-  const yourToken = localStorage.getItem('bus-token');
+  const yourToken = localStorage.getItem("bus-token");
   const [keyword, setKeyword] = useState("");
-  const [sort, setSort] = useState("");
+  const [sort, setSort] = useState("date_desc");
   const [page, setPage] = useState(1);
   const [pagination, setPagination] = useState({
     currentPage: 1,
@@ -35,27 +35,30 @@ export default function TicketListPage() {
   }, [keyword, sort, page]);
 
   function callApi({ keyword = "", sort = "", page = 1, limit = 10 }) {
-    axios.get(`${baseURL}/api/Ticket`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'ngrok-skip-browser-warning': 69420,
-        'Authorization': `Bearer ${yourToken}`
-      },
-      params: {
-        Keyword: keyword,
-        SortBy: sort,
-        Page: page,
-        PageSize: limit,
-      },
-    })
+    axios
+      .get(`${baseURL}/api/Ticket`, {
+        headers: {
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": 69420,
+          Authorization: `Bearer ${yourToken}`,
+        },
+        params: {
+          Keyword: keyword,
+          SortBy: sort,
+          Page: page,
+          PageSize: limit,
+        },
+      })
       .then((res) => {
         if (res.status === 200) {
           const listData = res.data?.data || [];
-          setDataList(listData.map((item) => ({
-            ...item,
-            createdAt: formatDateTime(item.createdAt),
-            price: formatToMoney(item.price),
-          })));
+          setDataList(
+            listData.map((item) => ({
+              ...item,
+              createdAt: formatDateTime(item.createdAt),
+              price: formatToMoney(item.price),
+            }))
+          );
           if (res.data?.pagination) {
             setPagination(res.data.pagination);
           }
@@ -64,18 +67,42 @@ export default function TicketListPage() {
         }
       })
       .catch((error) => {
-        console.error('Axios error:', error);
+        console.error("Axios error:", error);
         showError();
       });
   }
 
-  return (
-    <div className='flex flex-row w-full'>
-      <SidebarAdmin />
-      <div className='flex flex-col w-full'>
+  function handleConfirmBoarding(name, id) {
+    {
+      if (confirm(`Xâc nhận hành khách ${name} đã lên xe?`)) {
+        axios
+          .put(`${baseURL}/api/Staff/ticket/confirm-boarding/${id}`, {
+            headers: {
+              "Content-Type": "application/json",
+              "ngrok-skip-browser-warning": 69420,
+              Authorization: `Bearer ${yourToken}`,
+            },
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              showSuccess();
+            }
+            callApi({ keyword, sort, page });
+          })
+          .catch((e) => {
+            showError(e?.response?.data?.message);
+            callApi({ keyword, sort, page });
+          });
+      }
+    }
+  }
 
+  return (
+    <div className="flex flex-row w-full">
+      <SidebarAdmin />
+      <div className="flex flex-col w-full">
         <div className="flex justify-between items-center h-[60px] px-4 shadow-lg">
-          <h1 className='text-2xl font-bold'>Danh sách vé</h1>
+          <h1 className="text-2xl font-bold">Danh sách vé</h1>
         </div>
 
         <div className="flex justify-between items-center px-4 h-[64px]">
@@ -86,13 +113,13 @@ export default function TicketListPage() {
               placeholder="Nhập từ khoá tìm kiếm..."
               className="w-[300px] border border-primary rounded-lg py-2 px-4"
               onChange={(e) => setKeyword(e.target.value)}
-            /> <select
+            />{" "}
+            <select
               className="border border-primary rounded-lg py-2 px-4"
               onChange={(e) => setSort(e.target.value)}
             >
-              <option value="">Theo ngày tạo mới nhất</option>
+              <option value="date_desc">Theo ngày tạo mới nhất</option>
               <option value="date_asc">Theo ngày cũ nhất</option>
-              <option value="date_desc">Theo ngày tạo giảm dần</option>
               <option value="price_desc">Theo giá giảm dần</option>
             </select>
           </div>
@@ -104,7 +131,9 @@ export default function TicketListPage() {
           renderActions={(row) => (
             <div className="flex gap-2 justify-center">
               <button
-                onClick={() => alert(row.ticketId)}
+                onClick={() =>
+                  handleConfirmBoarding(row.customerName, row.ticketId)
+                }
                 className="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
               >
                 Xác nhận lên xe
@@ -131,6 +160,6 @@ export default function TicketListPage() {
           />
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
