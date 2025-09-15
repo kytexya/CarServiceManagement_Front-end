b36b69dcea0b5984d10b227f1bb491a8fa6b2ebf
ngynmikan <ngynmikan@gmail.com>
2025-09-15 22:09:02 +0700

    fix error on production

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel'
 import { fileURLToPath } from 'url';
 
 export default ({ mode }) => {
